d89a590e7d16602be77efa76a40232270ac21fc7
eya <Eya.HERMI@esprit.tn>
2026-04-22 21:51:54 +0100

    Fix final2 SonarCloud code smells

--- src/main/java/tn/esprit/clubservice/security/JwtUtils.java
+++ src/main/java/tn/esprit/clubservice/security/JwtUtils.java
@@ -11,7 +11,7 @@ import java.util.function.Function;
 @Component
 public class JwtUtils {
 
-    private static final String SECRET_KEY = "v9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v8y/B?E(G+KbPeShVmYp";
+    private static final String SECRET_KEY = "4n7p9r2t5v8y/B?E(H+KbPeShVmYq3t6w9z$C&F)J@NcQfTjWnZr4u7x!A%D*G-Ka";
     private static final SecretKey SIGNING_KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
     public String extractUsername(String token) {
